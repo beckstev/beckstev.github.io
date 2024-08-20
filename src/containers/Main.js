67a52fa6e37e560cccd01f67bf48a69b3d5936a2
becktev <steven.becker@mpl.mpg.de>
2024-08-20 19:30:20 +0200

    Pretified main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -15,7 +15,7 @@ import Education from "./education/Education";
 import ScrollToTopButton from "./topbutton/Top";
 import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
-import TalkCarousel from "./talks_as_carousel/TalkCarousel"
+import TalkCarousel from "./talks_as_carousel/TalkCarousel";
 import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
